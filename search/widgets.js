7381e1069ca9d444c8a606337d5330534f2c4183
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-01 12:34:39 -0400

    s

--- search/widgets.js
+++ search/widgets.js
@@ -1,7 +1,61 @@
+var widgets = [
+    {
+        "regex": ".*calculator.*",
+        "function": function(query) {
+            return `
+                <div>
+                    <input type="text" id="display" readonly>
+                    <div>
+                        <button onclick="appendToDisplay('1')">1</button>
+                        <button onclick="appendToDisplay('2')">2</button>
+                        <button onclick="appendToDisplay('3')">3</button>
+                        <button onclick="appendToDisplay('+')">+</button>
+                    </div>
+                    <div>
+                        <button onclick="appendToDisplay('4')">4</button>
+                        <button onclick="appendToDisplay('5')">5</button>
+                        <button onclick="appendToDisplay('6')">6</button>
+                        <button onclick="appendToDisplay('-')">-</button>
+                    </div>
+                    <div>
+                        <button onclick="appendToDisplay('7')">7</button>
+                        <button onclick="appendToDisplay('8')">8</button>
+                        <button onclick="appendToDisplay('9')">9</button>
+                        <button onclick="appendToDisplay('*')">*</button>
+                    </div>
+                    <div>
+                        <button onclick="clearDisplay()">C</button>
+                        <button onclick="appendToDisplay('0')">0</button>
+                        <button onclick="calculateResult()">=</button>
+                        <button onclick="appendToDisplay('/')">/</button>
+                    </div>
+                </div>
+                <script>
+                    function appendToDisplay(value) {
+                        document.getElementById('display').value += value;
+                    }
+                    function clearDisplay() {
+                        document.getElementById('display').value = '';
+                    }
+                    function calculateResult() {
+                        const display = document.getElementById('display');
+                        display.value = eval(display.value);
+                    }
+                </script>
+            `;
+        }
+    }     
+]
+
 function showwidget(query) {
     var html;
 
-    // code here
+    for (var i = 0; i < widgets.length; i++) {
+        if (query.match(widgets[i].regex)) {
+            html = widgets[i].function(query);
+            break;
+        }
+    }
 
     if (html) {
         displayQuickAnswer(html, true);
