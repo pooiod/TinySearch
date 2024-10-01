984d61bef6a7969b0dbf34a170b15dbcb99f0ebf
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-01 13:21:43 -0400

    u

--- search/widgets.js
+++ search/widgets.js
@@ -1,50 +1,50 @@
 var widgets = [
     {
-        "regex": ".*calculator.*",
+        "regex": ".*calculator.*|^[-+]?[0-9]*\\.?[0-9]+\\s*[-+*/]\\s*[-+]?[0-9]*\\.?[0-9]+$",
         "function": function(query) {
-            return `
-                <div>
-                    <input type="text" id="display" readonly>
+            const calculate = (equation) => {
+                try {
+                    return eval(equation);
+                } catch (error) {
+                    return 'Error';
+                }
+            };
+    
+            if (/calculator/i.test(query)) {
+                return `
                     <div>
-                        <button onclick="appendToDisplay('1')">1</button>
-                        <button onclick="appendToDisplay('2')">2</button>
-                        <button onclick="appendToDisplay('3')">3</button>
-                        <button onclick="appendToDisplay('+')">+</button>
+                        <input type="text" id="display" readonly>
+                        <div>
+                            <button onclick="appendToDisplay('1')">1</button>
+                            <button onclick="appendToDisplay('2')">2</button>
+                            <button onclick="appendToDisplay('3')">3</button>
+                            <button onclick="appendToDisplay('+')">+</button>
+                        </div>
+                        <div>
+                            <button onclick="appendToDisplay('4')">4</button>
+                            <button onclick="appendToDisplay('5')">5</button>
+                            <button onclick="appendToDisplay('6')">6</button>
+                            <button onclick="appendToDisplay('-')">-</button>
+                        </div>
+                        <div>
+                            <button onclick="appendToDisplay('7')">7</button>
+                            <button onclick="appendToDisplay('8')">8</button>
+                            <button onclick="appendToDisplay('9')">9</button>
+                            <button onclick="appendToDisplay('*')">*</button>
+                        </div>
+                        <div>
+                            <button onclick="clearDisplay()">C</button>
+                            <button onclick="appendToDisplay('0')">0</button>
+                            <button onclick="calculateResult()">=</button>
+                            <button onclick="appendToDisplay('/')">/</button>
+                        </div>
                     </div>
-                    <div>
-                        <button onclick="appendToDisplay('4')">4</button>
-                        <button onclick="appendToDisplay('5')">5</button>
-                        <button onclick="appendToDisplay('6')">6</button>
-                        <button onclick="appendToDisplay('-')">-</button>
-                    </div>
-                    <div>
-                        <button onclick="appendToDisplay('7')">7</button>
-                        <button onclick="appendToDisplay('8')">8</button>
-                        <button onclick="appendToDisplay('9')">9</button>
-                        <button onclick="appendToDisplay('*')">*</button>
-                    </div>
-                    <div>
-                        <button onclick="clearDisplay()">C</button>
-                        <button onclick="appendToDisplay('0')">0</button>
-                        <button onclick="calculateResult()">=</button>
-                        <button onclick="appendToDisplay('/')">/</button>
-                    </div>
-                </div>
-                <script>
-                    function appendToDisplay(value) {
-                        document.getElementById('display').value += value;
-                    }
-                    function clearDisplay() {
-                        document.getElementById('display').value = '';
-                    }
-                    function calculateResult() {
-                        const display = document.getElementById('display');
-                        display.value = eval(display.value);
-                    }
-                </script>
-            `;
+                `;
+            } else if (/^[-+]?[0-9]*\\.?[0-9]+\\s*[-+*/]\\s*[-+]?[0-9]*\\.?[0-9]+$/.test(query)) {
+                return calculate(query);
+            }
         }
-    }     
+    }       
 ]
 
 function showwidget(query) {
