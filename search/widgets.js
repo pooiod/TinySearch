c293306aaf5895a4616d333f7df4f22456828287
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-01 14:08:18 -0400

    o

--- search/widgets.js
+++ search/widgets.js
@@ -9,28 +9,28 @@ var widgets = [
                     <div>
                         <input type="text" id="display" readonly>
                         <div>
-                            <button onclick="window.appendToDisplay('1')">1</button>
-                            <button onclick="window.appendToDisplay('2')">2</button>
-                            <button onclick="window.appendToDisplay('3')">3</button>
-                            <button onclick="window.appendToDisplay('+')">+</button>
+                            <button onclick="appendToDisplay('1')">1</button>
+                            <button onclick="appendToDisplay('2')">2</button>
+                            <button onclick="appendToDisplay('3')">3</button>
+                            <button onclick="appendToDisplay('+')">+</button>
                         </div>
                         <div>
-                            <button onclick="window.appendToDisplay('4')">4</button>
-                            <button onclick="window.appendToDisplay('5')">5</button>
-                            <button onclick="window.appendToDisplay('6')">6</button>
-                            <button onclick="window.appendToDisplay('-')">-</button>
+                            <button onclick="appendToDisplay('4')">4</button>
+                            <button onclick="appendToDisplay('5')">5</button>
+                            <button onclick="appendToDisplay('6')">6</button>
+                            <button onclick="appendToDisplay('-')">-</button>
                         </div>
                         <div>
-                            <button onclick="window.appendToDisplay('7')">7</button>
-                            <button onclick="window.appendToDisplay('8')">8</button>
-                            <button onclick="window.appendToDisplay('9')">9</button>
-                            <button onclick="window.appendToDisplay('*')">*</button>
+                            <button onclick="appendToDisplay('7')">7</button>
+                            <button onclick="appendToDisplay('8')">8</button>
+                            <button onclick="appendToDisplay('9')">9</button>
+                            <button onclick="appendToDisplay('*')">*</button>
                         </div>
                         <div>
-                            <button onclick="window.clearDisplay()">C</button>
-                            <button onclick="window.appendToDisplay('0')">0</button>
-                            <button onclick="window.calculateResult()">=</button>
-                            <button onclick="window.appendToDisplay('/')">/</button>
+                            <button onclick="clearDisplay()">C</button>
+                            <button onclick="appendToDisplay('0')">0</button>
+                            <button onclick="calculateResult()">=</button>
+                            <button onclick="appendToDisplay('/')">/</button>
                         </div>
                     </div>
                         window.appendToDisplay = function(value) {
